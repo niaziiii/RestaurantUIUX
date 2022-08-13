f7d7e243abcfb1874c65cb1092bd30f2dc4f592e
Muhabat Khan Niazi <muhabatkhan03@gmail.com>
2022-08-13 15:25:57 -0700

    added till introduction section

--- src/containers/Intro/Intro.jsx
+++ src/containers/Intro/Intro.jsx
@@ -1,19 +1,37 @@
-import React  from "react";
-// import { meal } from "../../constants";
-import viddeo from './../../assets/meal.mp4'
-// import {FontAwesomeIcon} from '@fortawesome/free-solid-svg-icons'
+import { React, useState } from "react";
+import { meal } from "../../constants";
+import { BsPlayCircle, BsPauseCircle } from 'react-icons/bs'
 function Intro() {
-  // const [videoPlay, setvideoPlay] = useState(false);
-  let icon ='⏯'
+  const [videoPlay, setvideoPlay] = useState(false);
+  const videoHandler = (cond) => {
+    const videoContainer = document.querySelector('.app__Intro__video');
+    if (cond) {
+      videoContainer.play()
+      setvideoPlay(cond)
+    } else {
+      videoContainer.pause()
+      setvideoPlay(cond)
+    }
+  }
+
   return (
-    <div className="app__Intro">
-      <video  controls width='100%' height="100%" className="app__Intro__video"/>
-      <source src={viddeo} type="video/mp4"/>
-      <i>{icon}</i>
-      
+    <div className="app__Intro section__padding">
+      <video
+        onEnded={() => videoHandler(false)}
+        src={meal}
+        controls width='100%' height="100%"
+        className="app__Intro__video" />
+      <i>{
+        videoPlay ?
+          <BsPauseCircle fontSize={40} onClick={() => videoHandler(false)} /> :
+          <BsPlayCircle fontSize={40} onClick={() => videoHandler(true)} />
+      }
+      </i>
+
     </div>
 
   );
 }
 
-export default Intro;
+
+export default Intro
