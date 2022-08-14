e92bb6d949864bc3473a989a6f70f04924a5a53e
Muhabat Khan Niazi <muhabatkhanx@gmail.com>
2022-08-14 11:34:36 -0700

    Added Gallery Section

--- src/containers/Gallery/Gallery.jsx
+++ src/containers/Gallery/Gallery.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import Subheading from '../../components/Subheading/Subheading'
+import { images } from '../../constants'
+import {AiOutlineArrowLeft,AiOutlineArrowRight} from 'react-icons/ai'
+const imagesGallery = [images.gallery01,images.gallery02,images.gallery03,images.gallery04]
 
 function Gallery() {
+  const scrollRef = React.useRef(null);
+
+  const scroll = (direction) =>{
+    const {current} = scrollRef;
+    console.log(current);
+    console.log(direction);
+    if (direction === "left") {
+      current.scrollLeft -= 300;
+    }else{
+        current.scrollLeft += 300;
+    }
+  }
   return (
-    <div>Gallery</div>
+    <div className='app__Gallery section__padding'>
+      <div className="app__Gallery__content">
+        <Subheading title='Instagram'/>
+        <h1 className='headtext__cormorant'>Photo Gallery</h1>
+        <p className='p__opensans'>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Commodi sapiente eius debitis? Officia ipsum reiciendis eaque.</p>
+        <button className='custom__button'>view more</button>
+      </div>
+      <div className="app__Gallery__imgContainer" >
+        <div className="app__Gallery__imgContainer__images" ref={scrollRef}>
+          {imagesGallery.map((el,i) => {
+            return(
+              <div className="Gallery-img" key={i}>
+                <img src={el} alt="Gallery-Images" />
+              </div>
+            )
+          })}
+        </div>
+        <div className="app__Gallery__imgContainer__icons">
+          <AiOutlineArrowLeft  fontSize={30} onClick= {() => scroll('left')}/>
+          <AiOutlineArrowRight className='right-icon' fontSize={30} onClick= {() => scroll('right')}/>
+        </div>
+      </div>
+    </div>
   )
 }
 
